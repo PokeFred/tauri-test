5cd1214b8acf098b14a4ec29f560e93cb3cc01d9
PokeFred <Cedric.paelmke@gmx.de>
2024-07-24 20:27:29 +0200

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,21 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 
+use std::collections::HashMap;
+use reqwest::Response;
+
+#[tauri::command]
+async fn ttt() -> Result<String, ()> {
+    let response: Response = reqwest::get("https://httpbin.org/ip").await.unwrap();
+    let data: HashMap<String, String> = response
+        .json::<HashMap<String, String>>()
+        .await.unwrap();
+
+    Ok(serde_json::to_string(&data).unwrap())
+}
+
 pub fn run() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![ttt])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
