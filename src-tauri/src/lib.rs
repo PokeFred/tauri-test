1f0e742d3556aad81816680e8345771d76553e5c
PokeFred <Cedric.paelmke@gmx.de>
2024-07-28 23:54:16 +0200

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,82 +1,27 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 
-use std::collections::HashMap;
-use reqwest::{Client, Response, Error, header::HeaderMap, Request, Method, Url};
-use serde::{Serialize, Deserialize};
-use base64::{Engine, prelude::BASE64_STANDARD};
-/*
-#[tauri::command]
-async fn check_auth() -> Result<String, ()> {
-    let mut data: HashMap<String, String> = HashMap::new();
-    data.insert("lang".to_string(), "rust".to_string());
-    data.insert("body".to_string(), "json".to_string());
-
-    let client: Client = Client::new();
-    let response: Response = client
-        .post("http://127.0.0.1:3000")
-        .header("xxx-my-header", "aabbb001-aaaaabbbbbcccccdddddeeeee")
-        //.body(serde_json::to_string(&data).unwrap())
-        .body("the exact body that is sent")
-        .send()
-        .await
-        .unwrap();
-
-    let data: String = response.text().await.unwrap();
-
-    Ok(data)
-}
-*/
+use tauri::Manager;
+use tauri::{AppHandle, WebviewWindow};
 
 #[tauri::command]
-async fn fetch_test_one() -> Result<String, String> {
-    let client: Client = Client::new();
-    let response: Result<Response, Error> = client
-        .post("http://127.0.0.1:3000/test-one")
-        .send()
-        .await;
+async fn set_app_title(app: AppHandle, title: String) -> Result<(), ()> {
+    let window: WebviewWindow = app.get_webview_window("main").unwrap();
+    let _ = window.set_title(&title);
 
-    Ok(response.unwrap().text().await.unwrap())
-}
-
-#[tauri::command]
-async fn fetch_test_two() -> Result<String, String> {
-    let client: Client = Client::new();
-    let response: Result<Response, Error> = client
-        .post("http://127.0.0.1:3000/test-two")
-        .send()
-        .await;
-
-    Ok(response.unwrap().text().await.unwrap())
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct TTT {
-    message: String
-}
-
-#[tauri::command]
-async fn fetch_test_three() -> Result<String, String> {
-    let mut headers: HeaderMap = HeaderMap::new();
-    headers.insert("xxx-test-header", "xxx-test-value".parse().unwrap());
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-
-
-    let ddd: TTT = TTT { message: "aaa".to_string() };
-
-    let client: Client = Client::new();
-    let response: Result<Response, Error> = client
-        .post("http://127.0.0.1:3000/test-three")
-        .headers(headers)
-        .body(serde_json::to_string(&ddd).unwrap())
-        .send()
-        .await;
-
-    Ok(response.unwrap().text().await.unwrap())
+    Ok(())
 }
 
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![fetch_test_one, fetch_test_two, fetch_test_three])
+        .setup(|app| {
+            let window: WebviewWindow = app.get_webview_window("main").unwrap();
+            let _ = window.set_title("TTT");
+            let _ = window.center();
+            let _ = window.minimize();
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![set_app_title])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/routes/(public)/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api/core"
+
+    setInterval(async (): Promise<void> => {
+        await invoke<void>("set_app_title", { title: (new Date()) })
+    }, 1000)
+</script>
+
+<div>Hello World</div>
